1c3de3a98030cdcef4d7eb52c0a2a0c5d73f90d1
= <7rubengarcia7@gmail.com>
2021-03-01 15:40:48 -0800

    chore: add main scss file to storybook preview file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { SupernovaProvider } from '../src/contexts/supernova';
 
+import '../src/sass/main.scss';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
