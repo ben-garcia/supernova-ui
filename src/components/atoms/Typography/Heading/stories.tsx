bc9efbb0022ee80ce7deba9e74f950d3a2588960
= <7rubengarcia7@gmail.com>
2021-03-06 15:28:00 -0800

    chore: change heading stories to render h1 tag in all sizes

--- src/components/atoms/Typography/Heading/stories.tsx
+++ src/components/atoms/Typography/Heading/stories.tsx
@@ -19,40 +19,26 @@ Primary.args = {
   children: 'Supernova UI',
 };
 
+const sizes = [
+  'xxxxl',
+  'xxxl',
+  'xxl',
+  'xl',
+  'lg',
+  'md',
+  'sm',
+  'xs',
+  'xxs',
+  'xxxs',
+  'xxxxs',
+];
+
 export const All = () => (
   <div>
-    <Heading fontSize="xxxxl" lineHeight="xxxxl">
-      (xxxxl) Supernova UI
-    </Heading>
-    <Heading fontSize="xxxl" lineHeight="xxxl">
-      (xxxl) Supernova UI
-    </Heading>
-    <Heading fontSize="xxl" lineHeight="xxl">
-      (xxl) Supernova UI
-    </Heading>
-    <Heading fontSize="xl" lineHeight="xl" tag="h2">
-      (xl) Supernova UI
-    </Heading>
-    <Heading fontSize="lg" lineHeight="lg" tag="h3">
-      (lg) Supernova UI
-    </Heading>
-    <Heading fontSize="md" lineHeight="md" tag="h4">
-      (md) Supernova UI
-    </Heading>
-    <Heading fontSize="sm" lineHeight="sm" tag="h5">
-      (sm) Supernova UI
-    </Heading>
-    <Heading fontSize="xs" lineHeight="xs" tag="h5">
-      (xs) Supernova UI
-    </Heading>
-    <Heading fontSize="xxs" lineHeight="xs" tag="h5">
-      (xxs) Supernova UI
-    </Heading>
-    <Heading fontSize="xxxs" lineHeight="xs" tag="h5">
-      (xxxs) Supernova UI
-    </Heading>
-    <Heading fontSize="xxxxs" lineHeight="xs" tag="h6">
-      (xxxxs) Supernova UI
-    </Heading>
+    {sizes.map(size => (
+      <Heading fontSize={size} key={size} lineHeight={size} tag="h1">
+        {`Supernova UI (${size})`}
+      </Heading>
+    ))}
   </div>
 );
