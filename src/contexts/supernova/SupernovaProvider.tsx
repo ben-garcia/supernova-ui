7c7ba555617581ad1c65226b6ecab0cbd5f8c006
= <7rubengarcia7@gmail.com>
2021-03-10 09:34:10 -0800

    feat: add custom theme to SupernovaProvider

--- src/contexts/supernova/SupernovaProvider.tsx
+++ src/contexts/supernova/SupernovaProvider.tsx
@@ -3,16 +3,26 @@ import React from 'react';
 import { SupernovaProviderProps } from './types';
 import { ThemeProvider, ThemeProviderProps } from '../theme';
 import { theme as defaultTheme } from '../../theme';
+import { deepMergify } from '../../utils';
 
 /**
  * The top level provider that contains all other providers
  * needed for the components to function properly.
  */
 const SupernovaProvider = (props: SupernovaProviderProps) => {
-  const { theme = defaultTheme, children } = props;
+  const { theme, children } = props;
+  let themeToUse;
+
+  // use default theme as is when the user doesn't include custom theme
+  if (!theme) {
+    themeToUse = defaultTheme;
+  } else {
+    // extend the default theme to include custom theme values
+    themeToUse = deepMergify(defaultTheme, theme);
+  }
 
   return (
-    <ThemeProvider value={theme as ThemeProviderProps['value']}>
+    <ThemeProvider value={themeToUse as ThemeProviderProps['value']}>
       {children}
     </ThemeProvider>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import createClasses from './create-classes';
 import createElement from './create-element';
 import createStyles from './create-styles';
+import deepMergify from './deep-mergify';
 
-export { createClasses, createElement, createStyles };
+export { createClasses, createElement, createStyles, deepMergify };
