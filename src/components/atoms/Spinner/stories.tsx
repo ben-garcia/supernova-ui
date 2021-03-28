1d94790029225960b487e2561aff9726370f40f0
= <7rubengarcia7@gmail.com>
2021-03-28 09:46:48 -0700

    chore(spinner): add story for all sizes

--- src/components/atoms/Spinner/stories.tsx
+++ src/components/atoms/Spinner/stories.tsx
@@ -15,4 +15,15 @@ const Template: Story<SpinnerProps> = args => <Spinner {...args} />;
 
 export const Basic = Template.bind({});
 
+export const Sizes = () => (
+  <div className="_snui-flex">
+    <Spinner size="xxl" />
+    <Spinner size="xl" />
+    <Spinner size="lg" />
+    <Spinner size="md" />
+    <Spinner size="sm" />
+    <Spinner size="xs" />
+  </div>
+);
+
 Basic.args = {};
