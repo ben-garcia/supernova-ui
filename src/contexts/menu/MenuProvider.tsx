41ae2c2e31bbc1b70f5dfe4ca4389e97b33156b1
= <7rubengarcia7@gmail.com>
2021-04-26 14:56:17 -0700

    chore(context): update types for menu

--- src/contexts/menu/MenuProvider.tsx
+++ src/contexts/menu/MenuProvider.tsx
@@ -3,10 +3,17 @@ import { createContext } from 'react';
 import { Menu } from './types';
 
 const initialState: Menu = {
+  activeMenuItem: null,
+  changeActiveMenuItem: () => {},
   id: '',
   closeOnEsc: false,
   isOpen: false,
   onClose: () => {},
+  getMenuItemProps: () => {},
+  getMenuListProps: () => {},
+  getMenuButtonProps: () => {},
+  menuListRef: null,
+  menuButtonRef: null,
 };
 
 export const MenuContext = createContext<Menu>(initialState);
--- src/contexts/menu/types.ts
+++ src/contexts/menu/types.ts
@@ -1,9 +1,31 @@
-import { ReactNode } from 'react';
+import { ReactNode, RefObject } from 'react';
+
+import { ButtonProps } from '../../components/atoms/Button/types';
+import { MenuListProps } from '../../components/molecules/Menu/MenuList';
+import { MenuItemProps } from '../../components/molecules/Menu/MenuItem';
 
 export interface Menu {
-  id: string;
+  activeMenuItem: RefObject<HTMLButtonElement> | null;
+  changeActiveMenuItem: (
+    newMenuItem: RefObject<HTMLButtonElement> | null
+  ) => void;
   closeOnEsc?: boolean;
+  getMenuButtonProps: (
+    props: ButtonProps,
+    ref: RefObject<HTMLButtonElement>
+  ) => void;
+  getMenuItemProps: (
+    props: MenuItemProps,
+    ref: RefObject<HTMLButtonElement>
+  ) => void;
+  getMenuListProps: (
+    props: Omit<MenuListProps, 'children'>,
+    ref: RefObject<HTMLDivElement>
+  ) => void;
+  id: string;
   isOpen: boolean;
+  menuButtonRef: RefObject<HTMLButtonElement> | null;
+  menuListRef: RefObject<HTMLDivElement> | null;
   onClose: () => void;
 }
 
