0426271d14ee7a371734e0f5572d3ff75f73da24
= <7rubengarcia7@gmail.com>
2021-04-26 14:57:38 -0700

    feat(hooks): update context props for menu

--- src/hooks/use-menu.ts
+++ src/hooks/use-menu.ts
@@ -1,7 +1,53 @@
-import { useContext } from 'react';
+import React, { useCallback, useContext, useRef, useState } from 'react';
 
 import { MenuContext } from '../contexts/menu';
 import { MenuProps } from '../components/molecules/Menu/types';
+import { isFunction } from '../utils';
+
+/**
+ * credit https://github.com/chakra-ui/chakra-ui/blob/53d0d0cfec7b4404fd2bc123991352f81bd39a82/packages/react-utils/src/refs.ts#L34
+ */
+
+type ReactRef<T> =
+  | React.Ref<T>
+  | React.RefObject<T>
+  | React.MutableRefObject<T>;
+
+/**
+ * Assigns a value to a ref function or object
+ *
+ * @param ref the ref to assign to
+ * @param value the value
+ */
+function assignRef<T = any>(ref: ReactRef<T> | undefined, value: T) {
+  if (ref == null) return;
+
+  if (isFunction(ref)) {
+    (ref as any)(value);
+    return;
+  }
+
+  try {
+    // @ts-ignore
+    // eslint-disable-next-line
+    ref.current = value;
+  } catch (error) {
+    throw new Error(`Cannot assign value '${value}' to ref '${ref}'`);
+  }
+}
+
+/**
+ * Combine multiple React refs into a single ref function.
+ * This is used mostly when you need to allow consumers forward refs to
+ * internal components
+ *
+ * @param refs refs to assign to value to
+ */
+function mergeRefs<T>(...refs: (ReactRef<T> | undefined)[]) {
+  return (value: T) => {
+    refs.forEach(ref => assignRef(ref, value));
+  };
+}
 
 /**
  * Hooks that returns the Menu props
@@ -9,40 +55,58 @@ import { MenuProps } from '../components/molecules/Menu/types';
 const useMenuProvider = (props: MenuProps) => {
   const { id: propId, isOpen, onClose, closeOnEsc } = props;
 
-  const id = propId || `menu-${Math.random()}`;
-  /*
-  const [hasFeedbackText, setHasFeedbackText] = useState(false);
-  const [hasHelpText, setHasHelpText] = useState(false);
-  const getHelpTextProps = useCallback(
-    (helperTextProps = {}, forwardedRef = null) => ({
-      id: `${id}-helper-text`,
-      ...helperTextProps,
-      ref: mergeRefs(forwardedRef, node => {
-        if (!node) return;
-        setHasHelpText(true);
-      }),
+  const [id] = useState(propId || `menu-${Math.random()}`);
+  const menuButtonRef = useRef<HTMLButtonElement>(null);
+  const menuListRef = useRef<HTMLDivElement>(null);
+  const activeMenuItem = useRef<HTMLButtonElement>(null);
+
+  const changeActiveMenuItem = useCallback(
+    (newItem: React.RefObject<HTMLButtonElement>) => {
+      // @ts-ignore
+      activeMenuItem.current = newItem;
+    },
+    []
+  );
+
+  const getMenuButtonProps = useCallback(
+    (menuButtonProps = {}, forwardedRef = null) => ({
+      ...menuButtonProps,
+      ref: mergeRefs(forwardedRef, menuButtonRef, () => {}),
     }),
     [id]
   );
-  const getErrorMessageProps = useCallback(
-    (errorMessageProps = {}, forwardedRef = null) => ({
-      id: `${id}-feedback`,
-      ...errorMessageProps,
-      ref: mergeRefs(forwardedRef, node => {
-        if (!node) return;
-        setHasFeedbackText(true);
-      }),
-      'aria-live': 'polite',
-    }),
+  const getMenuItemProps = useCallback(
+    (menuItemProps = {}, forwardedRef = null) => {
+      return {
+        ...menuItemProps,
+        id: `${id}-button-${Math.random()}`,
+        ref: mergeRefs(forwardedRef, () => {}),
+      };
+    },
     [id]
   );
-	*/
+  const getMenuListProps = useCallback(
+    (menuListProps = {}, forwardedRef = null) => {
+      return {
+        ...menuListProps,
+        ref: mergeRefs(forwardedRef, menuListRef, () => {}),
+      };
+    },
+    [id]
+  );
 
   return {
-    id,
-    onClose,
-    isOpen,
+    activeMenuItem,
+    changeActiveMenuItem,
     closeOnEsc,
+    getMenuButtonProps,
+    getMenuItemProps,
+    getMenuListProps,
+    id,
+    isOpen,
+    menuButtonRef,
+    menuListRef,
+    onClose,
   };
 };
 
