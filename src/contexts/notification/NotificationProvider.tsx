88ca9576dbd2cb0b32011da1425eaa4d1271dbf3
= <7rubengarcia7@gmail.com>
2021-05-13 11:04:39 -0700

    chore: export 'NotificationProvider' as React component

---
+++ src/contexts/notification/NotificationProvider.tsx
@@ -0,0 +1,95 @@
+import React, { createContext } from 'react';
+
+import { Portal } from '../../components/atoms';
+import Notification from '../../components/molecules/Notification';
+import reducer from './reducer';
+import {
+  Notification as NotificationType,
+  NotificationAction,
+  NotificationState,
+} from './types';
+
+export const initialState: NotificationState = {
+  lists: {
+    bottom: [],
+    'bottom-left': [],
+    'bottom-right': [],
+    top: [],
+    'top-left': [],
+    'top-right': [],
+  },
+};
+
+export const NotificationContext = createContext<{
+  state: NotificationState;
+  dispatch: React.Dispatch<NotificationAction>;
+}>({
+  state: initialState,
+  dispatch: () => {},
+});
+
+interface NotificationProviderProps {
+  children: React.ReactNode;
+}
+
+export const NotificationProvider = (props: NotificationProviderProps) => {
+  const { children } = props;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const contextValue = React.useMemo(() => ({ state, dispatch }), [dispatch]);
+
+  return (
+    <NotificationContext.Provider value={contextValue}>
+      <Portal id="snui-notifications">
+        <div
+          className="snui-flex snui-flex-column snui-items-center snui-position-fixed snui-notification-bottom"
+          id="snui-notification-bottom"
+        >
+          {(state.lists.bottom as any).map((n: NotificationType) => (
+            <Notification {...n} key={n.id} />
+          ))}
+        </div>
+        <div
+          className="snui-flex snui-flex-column snui-position-fixed snui-notification-bottom-left"
+          id="snui-notification-bottom-left"
+        >
+          {(state.lists['bottom-left'] as any).map((n: NotificationType) => (
+            <Notification {...n} key={n.id} />
+          ))}
+        </div>
+        <div
+          className="snui-flex snui-flex-column snui-position-fixed snui-notification-bottom-right"
+          id="snui-notification-bottom-right"
+        >
+          {(state.lists['bottom-right'] as any).map((n: NotificationType) => (
+            <Notification {...n} key={n.id} />
+          ))}
+        </div>
+        <div
+          className="snui-flex snui-flex-column snui-items-center snui-position-fixed snui-notification-top"
+          id="snui-notification-top"
+        >
+          {(state.lists.top as any).map((n: NotificationType) => (
+            <Notification {...n} key={n.id} />
+          ))}
+        </div>
+        <div
+          className="snui-flex snui-flex-column snui-position-fixed snui-notification-top-left"
+          id="snui-notification-top-left"
+        >
+          {(state.lists['top-left'] as any).map((n: NotificationType) => (
+            <Notification {...n} key={n.id} />
+          ))}
+        </div>
+        <div
+          className="snui-flex snui-flex-column snui-position-fixed snui-notification-top-right"
+          id="snui-notification-top-right"
+        >
+          {(state.lists['top-right'] as any).map((n: NotificationType) => (
+            <Notification {...n} key={n.id} />
+          ))}
+        </div>
+      </Portal>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
--- src/contexts/supernova/SupernovaProvider.tsx
+++ src/contexts/supernova/SupernovaProvider.tsx
@@ -1,17 +1,10 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 
+import { NotificationProvider } from '../notification/NotificationProvider';
 import { SupernovaProviderProps } from './types';
 import { ThemeProvider, ThemeProviderProps } from '../theme';
 import { theme as defaultTheme } from '../../theme';
 import { deepMergify } from '../../utils';
-import Portal from '../../components/atoms/Portal';
-import {
-  NotificationContext,
-  initialState,
-} from '../notification/NotificationProvider';
-import reducer from '../notification/reducer';
-import { Notification as NotificationType } from '../notification/types';
-import Notification from '../../components/molecules/Notification';
 
 /**
  * The top level provider that contains all other providers
@@ -19,8 +12,6 @@ import Notification from '../../components/molecules/Notification';
  */
 const SupernovaProvider = (props: SupernovaProviderProps) => {
   const { theme, children } = props;
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const contextValue = React.useMemo(() => ({ state, dispatch }), [dispatch]);
   let themeToUse;
 
   // use default theme when a custom theme is not provided
@@ -32,61 +23,9 @@ const SupernovaProvider = (props: SupernovaProviderProps) => {
   }
 
   return (
-    <NotificationContext.Provider value={contextValue}>
-      <Portal id="snui-notifications">
-        <div
-          className="snui-flex snui-flex-column snui-items-center snui-position-fixed snui-notification-bottom"
-          id="snui-notification-bottom"
-        >
-          {(state.lists.bottom as any).map((n: NotificationType) => (
-            <Notification {...n} key={n.id} />
-          ))}
-        </div>
-        <div
-          className="snui-flex snui-flex-column snui-position-fixed snui-notification-bottom-left"
-          id="snui-notification-bottom-left"
-        >
-          {(state.lists['bottom-left'] as any).map((n: NotificationType) => (
-            <Notification {...n} key={n.id} />
-          ))}
-        </div>
-        <div
-          className="snui-flex snui-flex-column snui-position-fixed snui-notification-bottom-right"
-          id="snui-notification-bottom-right"
-        >
-          {(state.lists['bottom-right'] as any).map((n: NotificationType) => (
-            <Notification {...n} key={n.id} />
-          ))}
-        </div>
-        <div
-          className="snui-flex snui-flex-column snui-items-center snui-position-fixed snui-notification-top"
-          id="snui-notification-top"
-        >
-          {(state.lists.top as any).map((n: NotificationType) => (
-            <Notification {...n} key={n.id} />
-          ))}
-        </div>
-        <div
-          className="snui-flex snui-flex-column snui-position-fixed snui-notification-top-left"
-          id="snui-notification-top-left"
-        >
-          {(state.lists['top-left'] as any).map((n: NotificationType) => (
-            <Notification {...n} key={n.id} />
-          ))}
-        </div>
-        <div
-          className="snui-flex snui-flex-column snui-position-fixed snui-notification-top-right"
-          id="snui-notification-top-right"
-        >
-          {(state.lists['top-right'] as any).map((n: NotificationType) => (
-            <Notification {...n} key={n.id} />
-          ))}
-        </div>
-      </Portal>
-      <ThemeProvider value={themeToUse as ThemeProviderProps['value']}>
-        {children}
-      </ThemeProvider>
-    </NotificationContext.Provider>
+    <ThemeProvider value={themeToUse as ThemeProviderProps['value']}>
+      <NotificationProvider>{children}</NotificationProvider>
+    </ThemeProvider>
   );
 };
 
