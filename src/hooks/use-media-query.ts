da4b18c9e1a8072347744c3b1654627e2448a7e5
= <7rubengarcia7@gmail.com>
2021-05-14 13:33:48 -0700

    chore(hooks): call 'window' methods inside 'useEffect' to help ssr

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -6,11 +6,13 @@ import { useEffect, useState } from 'react';
  * @param mediaQuery query to check against the window
  */
 const useMediaQuery = (mediaQuery: string) => {
-  const [isMatch, setIsMatch] = useState<boolean>(
-    window.matchMedia(mediaQuery).matches
-  );
+  const [isMatch, setIsMatch] = useState<boolean>();
 
   useEffect(() => {
+    // setting this inside the useEffect will prevent it from running in node
+    // make sure that it only runs in the browser
+    setIsMatch(window.matchMedia(mediaQuery).matches);
+
     const mediaQueryList = window.matchMedia(mediaQuery);
     const changeHandler = () => setIsMatch(mediaQueryList.matches);
 
