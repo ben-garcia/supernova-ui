95684b549885b4250eeb55422a30b5f2a5934634
= <7rubengarcia7@gmail.com>
2021-06-02 09:13:40 -0700

    feat: export 'useBreakpoint' from index file

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './components/atoms';
 export * from './components/molecules';
 export { SupernovaProvider } from './contexts/supernova';
 export { useNotification } from './hooks/use-notification';
+export { default as useBreakpoint } from './hooks/use-breakpoint';
