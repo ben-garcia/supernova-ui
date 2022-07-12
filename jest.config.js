de98b989ae02db6a6c74b4db7046d582e955162a
ben <7rubengarcia7@gmail.com>
2022-07-12 15:58:13 -0700

    fix: configure test to work with absolute paths

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   // what files to include or exclude in the coverage report(regardless of test files)
   collectCoverageFrom: [
@@ -20,24 +24,20 @@ module.exports = {
     },
   },
 	*/
-  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
-    '\\.scss': '<rootDir>/src/__mocks__/styles.ts',
-    '\\.svg': '<rootDir>/src/__mocks__/svg.ts',
-    '@atoms': '<rootDir>/src/components/atoms',
-    '@atoms/(.*)': '<rootDir>/src/components/atoms/$1',
-    '@molecules': '<rootDir>/src/components/molecules',
-    '@molecules/(.*)': '<rootDir>/src/components/molecules/$1',
-    '@contexts': '<rootDir>/src/contexts',
-    '@hooks': '<rootDir>/src/hooks',
-    '@utils': '<rootDir>/src/utils',
-    '@testUtils': '<rootDir>/src/test-utils',
+    '\\.scss': '__mocks__/styles.ts',
+    '\\.svg': '__mocks__/svg.ts',
+    // convert tsconfig paths to a format that works in jest
+    ...pathsToModuleNameMapper(compilerOptions.paths),
   },
+  modulePaths: ['<rootDir>/src'],
+  // optional, you don't need it in case you use babel preset typescript
+  // preset: 'ts-jest',
   roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.storybook'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
-  setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.storybook'],
 };
