d1a87197589d71cfffa7ffab0ace8efca71ae0d4
ben <7rubengarcia7@gmail.com>
2022-08-07 15:40:43 -0700

    refactor(button): update responsive system

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,19 +1,19 @@
-import React, { forwardRef, useEffect, useState } from 'react';
+import React, { forwardRef } from 'react';
 
 import { Spinner } from '@atoms';
-import { useBreakpoint, useTheme } from '@hooks';
 import {
-  createClasses,
-  createStyles,
-  isFunction,
+  useClassStyles,
+  useCreateClassString,
+  useInlineStyles,
+  usePseudoClasses,
+} from '@hooks';
+import {
+  filterDataAriaListernerProps,
+  isObject,
   isString,
-  radii,
-  responsify,
-  shadows,
-  sizes,
+  validateDataAndAriaProps,
 } from '@utils';
 
-import { MarginPaddingProps, Sizes } from '@types';
 import { ButtonProps } from './types';
 import './styles.scss';
 
@@ -23,309 +23,60 @@ import './styles.scss';
 const Button = forwardRef((props: ButtonProps, ref: any) => {
   const {
     asSubmitButton = false,
-    backgroundColor = '',
-    borderRadius = '',
-    boxShadow = '',
     children,
-    className = '',
-    color = '',
-    font = 'body',
-    fontSize = '',
-    fontWeight = '',
-    height = '',
-    hoverBackgroundColor = '',
-    hoverColor = '',
+    className,
+    colorVariant,
     isDisabled = false,
     isLoading = false,
-    leftIcon = null,
-    letterSpacing = '',
-    lineHeight = '',
-    loadingText = '',
-    isTruncated = false,
-    margin = '',
-    onClick = () => {},
-    onFocus,
-    onMouseEnter = null,
-    onMouseLeave = null,
-    padding = '',
-    rightIcon = null,
-    spinner = null,
-    textTransform = '',
+    leftIcon,
+    loadingText,
+    rightIcon,
+    size = 'md',
+    spinner,
     variant = 'filled',
-    width = '',
+    _hover,
+    _focus,
     ...rest
   } = props;
-  const theme = useTheme();
-  const breakpoint = useBreakpoint();
-  const [focusRingColor, setFocusRingColor] = useState('');
-  const [backgroundColorToUse, setBackgroundColorToUse] = useState('');
-  const [colorToUse, setColorToUse] = useState('');
-
-  useEffect(() => {
-    if (isString(backgroundColor)) {
-      if ((theme as any).colors[backgroundColor]) {
-        setBackgroundColorToUse((theme as any).colors[backgroundColor]);
-      } else {
-        setBackgroundColorToUse(backgroundColor);
-      }
-    }
-    if (isString(color)) {
-      if ((theme as any).colors[color]) {
-        setColorToUse((theme as any).colors[color]);
-      } else {
-        setColorToUse(color);
-      }
-    }
-  }, []);
-
-  const classes = createClasses(
-    'snui-button snui-inline-flex snui-flex-center',
-    {
-      [`snui-border-radius-${borderRadius}`]:
-        isString(borderRadius) && radii.includes(borderRadius),
-      [`snui-box-shadow-${boxShadow}`]:
-        isString(boxShadow) && shadows.includes(boxShadow),
-      [className]: isString(className),
-      [`snui-font-${font}`]:
-        (font && font === 'heading') || font === 'body' || font === 'mono',
-      [`snui-text-${fontSize}`]:
-        isString(fontSize) && sizes.includes(fontSize as string),
-      [`snui-font-weight-${fontWeight}`]:
-        isString(fontWeight) && sizes.includes(fontWeight),
-      [`snui-height-${height}`]:
-        isString(height) && sizes.includes(height as string),
-      'snui-disabled': isDisabled || isLoading,
-      'snui-text-truncated': isTruncated,
-      [`snui-letter-spacing-${letterSpacing}`]:
-        isString(letterSpacing) && sizes.includes(letterSpacing),
-      [`snui-line-height-${lineHeight}`]:
-        lineHeight !== '' && sizes.includes(lineHeight),
-      // margin
-      [`snui-margin-${margin}`]:
-        typeof margin === 'string' && margin !== '' && sizes.includes(margin),
-      [`snui-margin-bottom-${(margin as MarginPaddingProps).bottom}`]:
-        typeof margin === 'object' &&
-        margin.bottom &&
-        typeof margin.bottom === 'string' &&
-        sizes.includes((margin as MarginPaddingProps).bottom as string),
-      [`snui-margin-left-${(margin as MarginPaddingProps).left}`]:
-        typeof margin === 'object' &&
-        margin.left &&
-        typeof margin.left === 'string' &&
-        sizes.includes((margin as MarginPaddingProps).left as string),
-      [`snui-margin-right-${(margin as MarginPaddingProps).right}`]:
-        typeof margin === 'object' &&
-        margin.right &&
-        typeof margin.right === 'string' &&
-        sizes.includes((margin as MarginPaddingProps).right as string),
-      [`snui-margin-top-${(margin as MarginPaddingProps).top}`]:
-        typeof margin === 'object' &&
-        margin.top &&
-        typeof margin.top === 'string' &&
-        sizes.includes((margin as MarginPaddingProps).top as string),
-      [`snui-margin-x-${(margin as MarginPaddingProps).x}`]:
-        // make sure that left and right properties have not been defined
-        !(margin as MarginPaddingProps).left &&
-        !(margin as MarginPaddingProps).right &&
-        typeof margin === 'object' &&
-        margin.x &&
-        typeof margin.y === 'string' &&
-        sizes.includes((margin as MarginPaddingProps).x as string),
-      [`snui-margin-y-${(margin as MarginPaddingProps).y}`]:
-        // make sure that top and bottom properties have not been defined
-        !(margin as MarginPaddingProps).bottom &&
-        !(margin as MarginPaddingProps).top &&
-        typeof margin === 'object' &&
-        margin.y &&
-        typeof margin.y === 'string' &&
-        sizes.includes((margin as MarginPaddingProps).y as string),
-      // padding
-      [`snui-padding-${padding}`]:
-        typeof padding === 'string' &&
-        padding !== '' &&
-        sizes.includes(padding),
-      [`snui-padding-bottom-${(padding as MarginPaddingProps).bottom}`]:
-        typeof padding === 'object' &&
-        padding.bottom &&
-        typeof padding.bottom === 'string' &&
-        sizes.includes((padding as MarginPaddingProps).bottom as string),
-      [`snui-padding-left-${(padding as any).left}`]:
-        typeof padding === 'object' &&
-        padding.left &&
-        sizes.includes((padding as MarginPaddingProps).left as string),
-      [`snui-padding-right-${(padding as MarginPaddingProps).right}`]:
-        typeof padding === 'object' &&
-        padding.right &&
-        typeof padding.right === 'string' &&
-        sizes.includes((padding as MarginPaddingProps).right as string),
-      [`snui-padding-top-${(padding as any).top}`]:
-        typeof padding === 'object' &&
-        padding.top &&
-        typeof padding.top === 'string' &&
-        sizes.includes((padding as MarginPaddingProps).top as string),
-      [`snui-padding-x-${(padding as MarginPaddingProps).x}`]:
-        !(padding as MarginPaddingProps).left &&
-        !(padding as MarginPaddingProps).right &&
-        typeof padding === 'object' &&
-        padding.x &&
-        typeof padding.x === 'string' &&
-        sizes.includes((padding as MarginPaddingProps).x as string),
-      [`snui-padding-y-${(padding as MarginPaddingProps).y}`]:
-        !(padding as MarginPaddingProps).bottom &&
-        !(padding as MarginPaddingProps).top &&
-        typeof padding === 'object' &&
-        padding.y &&
-        typeof padding.y === 'string' &&
-        sizes.includes((padding as MarginPaddingProps).y as string),
-      [`snui-text-${textTransform}`]:
-        textTransform === 'capitalize' ||
-        textTransform === 'lowercase' ||
-        textTransform === 'uppercase',
-      [`snui-button-${variant}`]: isString(variant),
-      [`snui-width-${width}`]:
-        isString(width) && sizes.includes(width as string),
-    }
-  );
-  const styles = createStyles(
-    {
-      backgroundColor,
-      borderRadius,
-      boxShadow,
-      color,
-      font,
-      fontWeight,
-      height,
-      letterSpacing,
-      lineHeight,
-      margin,
-      padding,
-      textTransform,
-      width,
-    },
-    theme,
-    breakpoint
-  );
-
-  responsify(
-    'paddingInline',
-    fontSize,
-    styles,
-    theme.spacing,
-    breakpoint,
-    sizes
-  );
-
-  responsify(
-    'height',
-    fontSize,
-    styles,
-    {
-      xs: `calc(${theme.spacing.xs} * 5)`,
-      sm: `calc(${theme.spacing.sm} * 5)`,
-      md: `calc(${theme.spacing.md} * 2)`,
-      lg: `calc(${theme.spacing.lg} * 2)`,
-      xl: `calc(${theme.spacing.xl} * 2)`,
-      xxl: `calc(${theme.spacing.xxl} * 2)`,
-    },
-    breakpoint,
-    sizes
-  );
-
-  if (isString(fontSize) && sizes.includes(fontSize as string)) {
-    if (fontSize === 'xs' || fontSize === 'sm') {
-      styles.paddingInlineStart = `${theme.spacing[fontSize as Sizes]}`;
-      styles.paddingInlineEnd = `${theme.spacing[fontSize as Sizes]}`;
-      styles.height = `calc(${theme.spacing[fontSize as Sizes]} * 5)`;
-    } else {
-      styles.paddingInlineStart = `${theme.spacing[fontSize as Sizes]}`;
-      styles.paddingInlineEnd = `${theme.spacing[fontSize as Sizes]}`;
-      styles.height = `calc(${theme.spacing[fontSize as Sizes]} * 2)`;
-    }
-  } else if (isString(fontSize) && !sizes.includes(fontSize as string)) {
-    styles.paddingInlineStart = fontSize as string;
-    styles.paddingInlineEnd = fontSize as string;
-    styles.height = `calc(${fontSize} * 2)`;
-  }
+  const pseudoClassName = usePseudoClasses({ _focus, _hover });
+  const stylesClassName = useClassStyles({
+    ...filterDataAriaListernerProps(rest),
+  });
+  const isPaddingTopNeeded =
+    !leftIcon && !rightIcon && !isLoading && !isObject(children);
+  const createInlineStyles = useInlineStyles(colorVariant);
+  const addClasses = useCreateClassString('snui-button', {
+    'snui-flex snui-items-center':
+      isObject(leftIcon) ||
+      isObject(rightIcon) ||
+      (isObject(spinner) && isLoading),
+    'snui-padding-top-sm': size === 'sm' && isPaddingTopNeeded,
+    'snui-padding-top-md': size === 'md' && isPaddingTopNeeded,
+    'snui-padding-top-lg': size === 'lg' && isPaddingTopNeeded,
+    [`snui-button--${variant}`]: isString(variant) && !props.backgroundColor,
+    [`snui-button--${size}`]:
+      isString(variant) && !props.height && !props.width,
+    [`${className}`]: isString(className),
+    [`${pseudoClassName}`]: isString(pseudoClassName),
+    [`${stylesClassName}`]: isString(stylesClassName),
+  });
 
   return (
     <button
-      {...rest}
-      className={classes}
+      {...validateDataAndAriaProps(rest)}
+      {...createInlineStyles()}
+      {...addClasses()}
+      aria-disabled={isDisabled || isLoading}
       disabled={isDisabled || isLoading}
-      onBlur={() => {
-        setFocusRingColor('');
-
-        if (isFunction(onMouseLeave)) {
-          onMouseLeave!();
-        }
-      }}
-      onClick={onClick}
-      onFocus={e => {
-        if (isFunction(onFocus)) {
-          onFocus!(e);
-        }
-        setFocusRingColor(theme.colors.focusRing);
-
-        if (isFunction(onMouseEnter)) {
-          onMouseEnter!(e as any);
-        }
-      }}
-      onMouseEnter={e => {
-        if ((theme as any).colors[hoverBackgroundColor]) {
-          setBackgroundColorToUse((theme as any).colors[hoverBackgroundColor]);
-        } else {
-          setBackgroundColorToUse(
-            isString(hoverBackgroundColor) ? hoverBackgroundColor : ''
-          );
-        }
-
-        if ((theme as any).colors[hoverColor]) {
-          setColorToUse((theme as any).colors[hoverColor]);
-        } else {
-          setColorToUse(isString(hoverColor) ? hoverColor : '');
-        }
-
-        if (isFunction(onMouseEnter)) {
-          onMouseEnter!(e);
-        }
-      }}
-      onMouseLeave={() => {
-        if (isString(backgroundColor)) {
-          if ((theme as any).colors[backgroundColor]) {
-            setBackgroundColorToUse((theme as any).colors[backgroundColor]);
-          } else {
-            setBackgroundColorToUse(backgroundColor);
-          }
-        } else {
-          setBackgroundColorToUse('');
-        }
-        if (isString(color)) {
-          if ((theme as any).colors[color]) {
-            setColorToUse((theme as any).colors[color]);
-          } else {
-            setColorToUse(color);
-          }
-        } else {
-          setColorToUse('');
-        }
-
-        if (isFunction(onMouseLeave)) {
-          onMouseLeave!();
-        }
-      }}
       ref={ref}
-      style={{
-        ...styles,
-        backgroundColor: backgroundColorToUse,
-        boxShadow: (isString(focusRingColor)
-          ? `0 0 0 4px ${focusRingColor}`
-          : null) as any,
-        color: colorToUse,
-      }}
       type={asSubmitButton ? 'submit' : 'button'}
     >
-      {leftIcon && !isLoading && leftIcon}
-      {!spinner && !isLoading && (leftIcon || rightIcon) && (
+      {leftIcon && !isLoading && (
+        <div style={{ height: size === 'sm' ? '0.8rem' : '1rem' }}>
+          {leftIcon}
+        </div>
+      )}
+      {!isLoading && (leftIcon || rightIcon) && (
         <span
           style={{
             margin: leftIcon || rightIcon ? '0.2rem 0.5rem 0 0.5rem' : '',
@@ -334,20 +85,45 @@ const Button = forwardRef((props: ButtonProps, ref: any) => {
           {children}
         </span>
       )}
-      {!spinner && !isLoading && !leftIcon && !rightIcon && <>{children}</>}
+      {/* when rendering with icon */}
+      {!isLoading && !isObject(leftIcon) && !isObject(rightIcon) && (
+        <div style={{ height: size === 'sm' ? '0.9rem' : '1.5rem' }}>
+          {children}
+        </div>
+      )}
+      {/* use default spinner */}
       {!spinner && isLoading && (
         <Spinner
-          size={fontSize || 'sm'}
+          height={size === 'sm' ? '0.8rem' : '1rem'}
+          width={size === 'sm' ? '0.8rem' : '1rem'}
           primaryColor={variant === 'filled' ? '#fff' : '#000'}
         />
       )}
-      {spinner && spinner}
+      {/* treat 'spinner' prop as the spinner */}
+      {spinner && isLoading && (
+        <div
+          style={{
+            // animation: 'snui-rotate 1s inifinte linear',
+            animationDuration: '1s',
+            animationIterationCount: 'infinite',
+            animationName: 'snui-rotate',
+            animationTimingFunction: 'linear',
+            height: size === 'sm' ? '0.8rem' : '1rem',
+          }}
+        >
+          {spinner}
+        </div>
+      )}
       {isLoading && isString(loadingText) && (
         <span className="snui-self-center snui-margin-left-sm">
           {loadingText}
         </span>
       )}
-      {rightIcon && !isLoading && rightIcon}
+      {rightIcon && !isLoading && (
+        <div style={{ height: size === 'sm' ? '0.8rem' : '1rem' }}>
+          {rightIcon}
+        </div>
+      )}
     </button>
   );
 });
