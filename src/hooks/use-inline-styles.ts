60d602914906cab731305c0edd31cf7e708b3942
ben <7rubengarcia7@gmail.com>
2022-08-10 08:44:50 -0700

    feat(use-inline-style): set 'color' based on the darkness of 'backgroundColor'

--- src/hooks/use-inline-styles.ts
+++ src/hooks/use-inline-styles.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 
 import { useTheme } from '@hooks';
+import { isDarkColor } from '@utils';
 
 import { ColorVariant } from '@types';
 
@@ -11,7 +12,12 @@ export const useInlineStyles = (colorVariant: ColorVariant | undefined) => {
   const { colors } = useTheme();
 
   const styles = colorVariant
-    ? { background: colors[colorVariant] }
+    ? {
+        background: colors[colorVariant],
+        color: isDarkColor(colors[colorVariant])
+          ? 'var(--snui-color-white)'
+          : 'var(--snui-color-black)',
+      }
     : undefined;
 
   return useCallback(() => ({ style: styles }), [colorVariant]);
