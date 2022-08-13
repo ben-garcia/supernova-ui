d885dd4eded09f35bf57dc6ae4b3a3eb56498224
ben <7rubengarcia7@gmail.com>
2022-08-13 07:02:07 -0700

    chore(form-control): add 'RadioGroup' to stories

--- src/components/molecules/FormControl/stories.tsx
+++ src/components/molecules/FormControl/stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import { TextInput, Textarea } from '@atoms';
+import { TextInput, Textarea, RadioGroup, Radio } from '@atoms';
 import { FormControl, FormHelperText, FormErrorMessage } from '@molecules';
 
 export default {
@@ -52,3 +52,18 @@ const WithTextInputTemplate: ComponentStory<typeof FormControl> = args => (
 
 export const WithTextInput = WithTextInputTemplate.bind({});
 WithTextInput.parameters = parameters;
+
+const WithRadioGroupTemplate: ComponentStory<typeof FormControl> = args => (
+  <FormControl {...args}>
+    <RadioGroup defaultValue="3.14159" name="answer">
+      <Radio label="3.14195" value="3.14195" />
+      <Radio label="3.15149" value="3.15249" />
+      <Radio label="3.14159" value="3.14159" />
+    </RadioGroup>
+    <FormHelperText>Choose the correct value of PI</FormHelperText>
+    <FormErrorMessage>error has been detected</FormErrorMessage>
+  </FormControl>
+);
+
+export const WithRadioGroup = WithRadioGroupTemplate.bind({});
+WithRadioGroup.parameters = parameters;
