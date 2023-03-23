4fb346a7d18ec251dc824a1e06bdf7eb47099a06
ben <7rubengarcia7@gmail.com>
2023-03-23 11:05:48 -0700

    feat(alertdialog): extract overlay to a separate component

---
+++ src/components/molecules/AlertDialog/AlertDialogOverlay/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import { Overlay } from '@atoms';
+import { useAlertDialog } from '@hooks';
+
+/**
+ * The overlay for the AlertDialog component.
+ */
+const AlertDialogOverlay: FC<Parameters<typeof Overlay>[0]> = props => {
+  const { isOpen } = useAlertDialog();
+
+  return isOpen ? <Overlay {...props} /> : null;
+};
+
+export default AlertDialogOverlay;
