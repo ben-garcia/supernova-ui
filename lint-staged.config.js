869f48e547090f8c7a9298562e0826a9e2e00698
ben <7rubengarcia7@gmail.com>
2024-08-10 11:31:17 -0700

    chore: use npm to run lint-staged tasks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'yarn type-check',
+  '**/*.ts?(x)': () => 'npm run type-check',
   // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': filenames => `yarn lint ${filenames.join(' ')}`,
+  '**/*.(ts|js)?(x)': filenames => `npm run lint ${filenames.join(' ')}`,
   // Run Stylelint on changes to SCSS files
-  '**/*.scss': filenames => `yarn lint:scss ${filenames.join(' ')}`,
+  '**/*.scss': filenames => `npm run lint:scss ${filenames.join(' ')}`,
   // Run prettier on changes to js, ts, tsx, json, md
-  '**/*.(js|ts|tsx|json|md)': filenames => `yarn format ${filenames.join(' ')}`,
+  '**/*.(js|ts|tsx|json|md)': filenames =>
+    `npm run format ${filenames.join(' ')}`,
   // Run jest on changes to ts, tsx
   '**/*.test.(ts|tsx)': filenames =>
-    `yarn test --passWithNoTests ${filenames.join(' ')}`,
+    `npm run test --passWithNoTests ${filenames.join(' ')}`,
 };
