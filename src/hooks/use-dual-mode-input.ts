1a14eebd88ee35537748fb90e7282d10c6c00739
Ben <7rubengarcia7@gmail.com>
2026-05-09 08:45:32 -0700

    refactor: imporove type inference

--- src/hooks/use-dual-mode-input.ts
+++ src/hooks/use-dual-mode-input.ts
@@ -1,14 +1,16 @@
 import { useEffect, useRef, useState } from 'react';
 
-interface UseDualModeInputProps {
+type InputValue = boolean | string | number | readonly string[] | string[];
+
+interface UseDualModeInputProps<T extends InputValue = InputValue> {
   /**
    * The current value of state. Indicates controlled mode.
    */
-  value?: boolean | string | number | readonly string[];
+  value?: T;
   /**
    * The initial value of the input. Indicates uncontrolled mode.
    */
-  defaultValue?: boolean | string | number | readonly string[];
+  defaultValue?: T;
   /**
    * Name of the input component.
    */
@@ -19,43 +21,44 @@ interface UseDualModeInputProps {
  * Hook that detects whether form input fields are used in controlled or
  * uncontrolled mode.
  */
-export function useDualModeInput({
+export function useDualModeInput<T extends InputValue = InputValue>({
   value: controlled,
   defaultValue,
   name,
-}: UseDualModeInputProps) {
+}: UseDualModeInputProps<T>) {
   // Determine if this is controlled or uncontrolled mode
   const isControlled = controlled !== undefined;
+
   // Internal state for uncontrolled mode
-  const [internalValue, setInternalValue] = useState<
-    boolean | string | number | readonly string[] | undefined
-  >(defaultValue);
+  const [internalValue, setInternalValue] = useState<T | undefined>(
+    defaultValue
+  );
+
   // Use value if controlled, otherwise use internal state
   const currentValue = isControlled ? controlled : internalValue;
+
   // Move this to the top level
   const wasControlledRef = useRef(isControlled);
 
   // Validate against using both controlled and uncontrolled modes.
   useEffect(() => {
     if (controlled !== undefined && defaultValue !== undefined) {
-      // eslint-disable-next-line
       console.warn(
         `<${name}>: Do not specify both "value/checked" and "defaultChecked/defaultValue".
           Use "value/checked" for controlled mode or "defaultChecked/defaultValue" for uncontrolled mode.`
       );
     }
-  }, [controlled, defaultValue]);
+  }, [controlled, defaultValue, name]);
 
   // Mode switching validation
   useEffect(() => {
     if (wasControlledRef.current !== isControlled) {
-      // eslint-disable-next-line
       console.warn(
         `<${name}> switched from controlled to uncontrolled or vice versa`
       );
     }
     wasControlledRef.current = isControlled;
-  }, [isControlled]);
+  }, [isControlled, name]);
 
   return { value: currentValue, setInternalValue, isControlled };
 }
